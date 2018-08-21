a9da38ea25f9da1f39afd8f18ae61bab26c592c0
Prabin Pun <prabin1punah@gmail.com>
2018-08-21 10:56:35 -0400

    Add project files.

---
+++ SpaceGame/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            gameTitle();
+            first();
+        }
+
+        public static void gameTitle()
+        {
+            Console.WriteLine("Welcome to SpaceGame-1 (-_-)");
+            Console.WriteLine("Every Great Journey Begins With a Dream! ");
+            Console.Write("Enter your First Name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Enter your Last Name: ");
+            myLastName = Console.ReadLine();
+
+            Console.Write("Welcome " + myFirstName + " " + "LOSER------------ (-_-) !" );
+            Console.ReadLine();
+      
+        }
+
+        public static void first()
+        {
+
+        }
+
+        public static void second()
+        {
+
+        }
+
+        public static void third()
+        {
+
+        }
+
+        public static void YouWin()
+        {
+
+        }
+
+        public static void GameOver()
+        {
+
+        }
+
+    }   
+}
+
+
+
+
+
+
+
+
+
+/*
+case 1 
+    Planet A
+    Ship A
+    Carryon A
+
+case 2
+    Planet A
+    Ship B
+    Carryon  B
+
+case 3
+    Planet A
+    Ship B
+    Carryon A
+
+case 4
+    Planet A
+    Ship A
+    Carryon B
+
+
+case 5
+    Planet B
+    Ship B
+    Carryon B
+
+case 6
+    Planet B
+    Ship A
+    Carryon B
+
+case 7
+    Planet B
+    Ship B
+    Carryon A
+    */
+
+
