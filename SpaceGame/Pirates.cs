a2b6223e31999b9c69cb13fb2372dff16cccb572
Prabin Pun <prabin1punah@gmail.com>
2018-08-29 20:17:58 -0400

    commit

--- SpaceGame/Pirates.cs
+++ SpaceGame/Pirates.cs
@@ -10,6 +10,18 @@ namespace SpaceGame
     {
         public Pirates()
         {
+            Console.WriteLine("Warning! Warning!");
+            Console.WriteLine("Space Pirates! Space Pirates!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Our radar has detected Space Pirates.");
+            Console.WriteLine("Do you want to engage, or turn around and go different direction. ");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+
 
         }
     }
