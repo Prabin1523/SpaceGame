cef6b4732837b11219f3f153717afd604783b6db
Prabin Pun <prabin1punah@gmail.com>
2018-08-30 14:30:42 -0400

    commit

---
+++ SpaceGame/DistanceTravelled.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    class DistanceTravelled
+    {
+
+        public static double travelTime(double W)
+        {
+            double travelTime = 0;
+            double distance = 0;
+
+            travelTime = distance / (Math.Pow(W, 10 / 3) + Math.Pow(10-W, -11 / 3));
+            return travelTime;
+
+         
+        }
+    }
+}
